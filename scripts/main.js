e5f1e5a09377a8d621f6cd2a0b0c78f7ed23706b
Claudia Maciel-Contreras <clc_80@yahoo.com>
2017-04-02 18:27:56 -0700

    Added hover transition to side images

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var ESC_KEY  = 27;
 
 //Will change the detail image and the detail image title
 function setDetails(imageUrl, titleText) {
@@ -36,6 +38,7 @@ function addThumbClickHandler(thumb){
   thumb.addEventListener('click', function(event){
     event.preventDefault();
     setDetailsFromThumb(thumb);
+    showDetails();
   });
 }
 
@@ -46,12 +49,34 @@ function getThumbnailsArray(){
   var thumbnailArray = [].slice.call(thumbnails);
   return thumbnailArray;
 }
+//add a class name to the body element
+function hideDetails() {
+  'use strict';
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+//making the detial image visible again
+function showDetails() {
+  'use strict';
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+}
+//Listens for a keyup event to hide the detail image to hideDetails
+function addKeyPressHandler() {
+  'use strict';
+  document.body.addEventListener('keyup', function(event){
+    event.preventDefault();
+    console.log(event.keyCode);
+    if(event.keyCode === ESC_KEY){
+      hideDetails();
+    }
+  });
+}
 
 //tie together all elements
 function initializeEvents() {
   'use strict';
   var thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
+  addKeyPressHandler();
 }
 
 initializeEvents();
